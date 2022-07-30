1217abebb92c16be2047297e085735719d4cb104
Quý Hoàng <quyhx@mor.com.vn>
2022-07-30 14:20:37 +0700

    4 Hashes and Caching

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,29 @@
 <?php
 
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Redis;
 
-Route::get('articles/trending', function () {
-    $trending = Redis::zrevrange('trending_articles',0,2) ; 
-    $trending = App\Article::hydrate(
-        array_map('json_decode',$trending)
-    );
-    dd($trending);
+Route::get('/', function () {
+//     $user3Stats=[
+//                   "favorites"	=>$user->favorites()->count(),
+//                   "watchLaters"	=>$user->watchLaters()->count(),
+//                   "completion"	=>$user->completion()->count()
+//                ];
+//     Redis::hmset('user.3.stats',$user3Stats);
+
+    // return Redis::hgetall('user.1.stats'); 
+
+
+
+Cache::put('foo',['name'=>'quy','age'=> 27],10);
+
+return cache::get('foo');
+
 });
-Route::get('articles/{article}', function (App\Article $article ) {
-    Redis::zincrby('trending_articles',1,$article);
-    return $article;
+
+Route::get('favorite-video',function(){
+
+     Redis::hincrby('user.1.stats','favorites',1);
+     return redirect('/');
 });
+
