00d6c97e99289865b932d613d8b17cf7a1ce8b82
Quý Hoàng <quyhx@mor.com.vn>
2022-07-31 22:33:29 +0700

    Tái cấu trúc code cho project để dễ scale khi project trở nên phức tạp

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,33 @@
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Redis;
 
-Route::get('/',function(){
-    return Cache::remember ('articles.all',60*60,function(){
+
+class CacheableArticles
+{
+    protected $articles;
+
+    public function __construct(Articles $articles)
+    {   
+        $this->articles=$articles;
+    }
+
+    public function all()
+    {   
+        return Cache::remember ('articles.all',60*60,function(){
+            return  $this->articles->all();
+        });
+    }
+}
+class Articles
+{  
+    public function all()
+    {
         return App\Article::all();
-    });
+    }
+}
+
+Route::get('/',function(){
+
+    $articles = new CacheableArticles(new Articles);
+    return $articles->all();
 });
