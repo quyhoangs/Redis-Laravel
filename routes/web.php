0901bb5e2e0f47781559a60e963ebe200aa691ff
Quý Hoàng <quyhx@mor.com.vn>
2022-07-31 17:37:54 +0700

    Caching With Redis

--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,8 @@
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Redis;
 
-Route::get('/', function () {
-//     $user3Stats=[
-//                   "favorites"	=>$user->favorites()->count(),
-//                   "watchLaters"	=>$user->watchLaters()->count(),
-//                   "completion"	=>$user->completion()->count()
-//                ];
-//     Redis::hmset('user.3.stats',$user3Stats);
-
-    // return Redis::hgetall('user.1.stats'); 
-
-
-
-Cache::put('foo',['name'=>'quy','age'=> 27],10);
-
-return cache::get('foo');
-
+Route::get('/',function(){
+    return Cache::remember ('articles.all',60*60,function(){
+        return App\Article::all();
+    });
 });
-
-Route::get('favorite-video',function(){
-
-     Redis::hincrby('user.1.stats','favorites',1);
-     return redirect('/');
-});
-
